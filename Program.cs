76ec0961d94531a56717dfaad1d89944be35ede1
yulmaso <plotnik97@mail.ru>
2018-01-15 23:04:33 +0300

    Working_version

--- Program.cs
+++ Program.cs
@@ -8,122 +8,149 @@ namespace mlita_homework_2
     class Program
     {
         private static int position;
-
-        private static string data;
         
         private static string ch;
 
+        private static string letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
         private static void Expression()
         {
-            if (ch.Length > 2 && ch[position] == 'Т' && ch[position + 1] == 'а')
+            Header();
+            if (ch[position] == '_')
+                Read();
+            else
+                Error();
+            if (ch[position] == '-')
             {
-                Read(2);
+                Read();
                 Text();
+                EndChecker();
             }
+            else
+                Error();
+        }
+
+        private static void Header()
+        {
+            UpperCase();
+            Text();
+            if (ch[position] == ':')
+                Read();
+            else
+                Error();
+            if (ch[position] == '|')
+                Read();
+            else
+                Error();
+        }
+
+        private static void UpperCase()
+        {
+            if (letters.ToUpper().Contains(ch[position]))
+                Read();
             else
                 Error();
         }
 
         private static void Text()
         {
-            if (ch[position] == '.')
-                dotSymbol();
-            else if (position + 1 >= ch.Length)
-                Error();
-            else
+            if (letters.Contains(ch[position]))
             {
-                beginningSymbols();
-                afterText();
-            }
-        }
-
-        private static void beginningSymbols()
-        {
-            if ((ch[position] == 'т' || ch[position] == 'м') && ch[position + 1] == 'а')
-                Read(2);
-        }
-
-        private static void afterText()
-        {
-            if (ch[position] == '.')
-                dotSymbol();
-            else if (position + 1 >= ch.Length)
-                Error();
-            else if ((ch[position] == 'т' || ch[position] == 'м') && ch[position + 1] == 'а')
-                Text();
-            else if (ch[position] == 'т' || ch[position] == 'м')
-                endingSymbol();
-            else if (ch[position] == ';')
-                nextString();
-            else
-                Error();
-        }
-
-        private static void endingSymbol()
-        {
-            Read(1);
-            Text();    
-        }
-
-        private static void nextString()
-        {
-            if (position + 1 >= ch.Length)
-                Error();
-            else if (ch[position + 1] == '|')
-            {
-                Read(2);
-                Space();
-            }
-            else
-                Error();
-        }
-
-        private static void Space()
-        {
-            if (position >= ch.Length)
-                Error();
-            else if (ch[position] == '_')
-            {
-                Read(1);
-                afterSpace();
-            }
-            else
-                Error();
-        }
-        
-        private static void afterSpace()
-        {
-            if (position >= ch.Length)
-                Error();
-            else if (ch[position] == '_')
-                Space();
-            else if (position + 1 >= ch.Length)
-                Error();
-            else if(ch[position] == '-' && ch[position + 1] == ' ')
-            {
-                Read(2);
+                Read();
                 Text();
             }
+        }
+
+        private static void EndChecker()
+        {
+            if (ch[position] == ';')
+            {
+                Read();
+                if (ch[position] == '|')
+                    Read();
+                else
+                    Error();
+                if (ch[position] == '_')
+                    Read();
+                else
+                    Error();
+                Layer_1();
+            }
+            else if (ch[position] == '.')
+            {
+                Read();
+                Console.WriteLine("Строка принадлежит грамматике данного языка");
+            }
             else
                 Error();
         }
 
-        private static void dotSymbol()
+        private static void Layer_1()
         {
-            Read(1);
-            Console.WriteLine("Строка принадлежит грамматике данного языка");
+            if (ch[position] == '-')
+            {
+                Read();
+                Text();
+                EndChecker();
+            }
+            else if (ch[position] == '_')
+            {
+                Read();
+                Layer_2();
+            }
+            else
+                Error();
+        }
+
+        private static void Layer_2()
+        {
+            if (ch[position] == '-')
+            {
+                Read();
+                Text();
+                EndChecker();
+            }
+            else if (ch[position] == '_')
+            {
+                Read();
+                Layer_3();
+            }
+            else
+                Error();
+        }
+
+        private static void Layer_3()
+        {
+            if (ch[position] == '-')
+            {
+                Read();
+                Text();
+                EndChecker();
+            }
+            else
+                Error();
         }
 
         private static void Error()
         {
             Console.WriteLine("Введенное выражение не соответствует грамматике языка");
+            Console.WriteLine("Проверим ещё одну строку? Y/n");
+            ch = Console.ReadLine();
+            if (ch == "Y")
+            {
+                start();
+            }
+            else if (ch == "n")
+            {
+                Environment.Exit(0);
+            }
+            
         }
         
         //по идее эта функция должна записывать текущие символы в память, но в задании этого не требуется, поэтому она отвечает только за счетчик
-        private static void Read(int number)
+        private static void Read()
         {
-            position += number;
+            position++;
         }
         
         private static void grammaticRules()
@@ -131,23 +158,22 @@ namespace mlita_homework_2
             Console.WriteLine("Списки с тремя уровнями вложенности. Выполнил Плотников А.А. группа 6371 \n");
             Console.WriteLine("________________________________________________________________________");
             Console.WriteLine("Пример: (| -перевод строки, _ - пробел)");
-            Console.WriteLine("Татам;|_ - там;|_- тама;|__- ма;|__- та;|_- тата;|__- тат;|__-мам.\n");
-            Console.WriteLine("Правило ввода:");
-            Console.WriteLine("<выражение> ::= 'Та'<текст>");
-            Console.WriteLine("<текст> ::= <начальные символы><после текста>");
-            Console.WriteLine("<начальные символы> ::= 'та' || 'ма'");
-            Console.WriteLine("<после текста> ::= <текст> || <конечный сивол><точка с запятой, перевод строки> || <точка>");
-            Console.WriteLine("<конечный символ> ::= 'т' || 'м' || lambda");
-            Console.WriteLine("<точка с запятой, перевод строки> ::= ';|'<пробел>");
-            Console.WriteLine("<пробел> ::= “_”<после пробела>");
-            Console.WriteLine("<после пробела> ::= <пробел> || “- ”<текст>");
-            Console.WriteLine("<точка> ::= “.”");
+            Console.WriteLine("Татам:|_ - там;|_- тама;|__- ма;|__- та;|_- тата;|__- тат;|__-мам.\n");
+            Console.WriteLine("Грамматика языка:");
+            Console.WriteLine("<выражение> ::= <заголовок> _- <текст><проверка на конец>");
+            Console.WriteLine("<заголовок> ::= <большая буква><текст> :|");
+            Console.WriteLine("<большая буква> ::= А|…|Я");
+            Console.WriteLine("<текст> ::= <буква><текст> | Λ");
+            Console.WriteLine("<буква> ::= а| …|я");
+            Console.WriteLine("<проверка на конец> ::= ;|_ <уровень_1> | .");
+            Console.WriteLine("<уровень_1> ::= - <текст><проверка на конец> | _ <уровень_2>");
+            Console.WriteLine("<уровень_2> ::= - <текст><проверка на конец> | _<уровень_3>");
+            Console.WriteLine("<уровень_3> ::= - <текст><проверка на конец>");
             Console.WriteLine("________________________________________________________________________\n");
         }
 
-        static void Main(string[] args)
+        static void start()
         {
-            grammaticRules();
             while (true)
             {
                 Console.WriteLine("\nВведите выражение одной строкой:");
@@ -160,7 +186,13 @@ namespace mlita_homework_2
                 if ((ch = Console.ReadLine()) != "Y")
                     break;
             }
+        
         }
 
+        static void Main(string[] args)
+        {
+            grammaticRules();
+            start();
+        }
     }
 }
